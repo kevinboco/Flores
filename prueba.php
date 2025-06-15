766a801ca5f51bc34005b11b425b03ace429b87b
kevinboco <kevinalexanderboca@gmail.com>
2025-06-14 21:30:09 -0500

    IA

--- prueba.php
+++ prueba.php
@@ -11,7 +11,10 @@ $imagenes = [
   'girasoles' => 'https://images.pexels.com/photos/931179/pexels-photo-931179.jpeg',
   'RAMOS BUCHONES  ' => 'https://images.pexels.com/photos/1903962/pexels-photo-1903962.jpeg',
   'ROSAS EN FORMA DE CORAZON' => 'https://images.pexels.com/photos/356286/pexels-photo-356286.jpeg',
-  'Rosas' => 'https://images.pexels.com/photos/931162/pexels-photo-931162.jpeg',
+  'rosas' => 'https://images.pexels.com/photos/931162/pexels-photo-931162.jpeg',
+  'RAMOS CON LUCES' => 'https://images.pexels.com/photos/1903962/pexels-photo-1903962.jpeg',
+   '60000' => 'https://images.pexels.com/photos/931179/pexels-photo-931179.jpeg',
+
 ];
 ?>
 <!DOCTYPE html>
